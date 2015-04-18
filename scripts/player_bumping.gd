726da24824edd005f69be0746067d5bb4420df15
MartiniMoe <martini.moe@cheex.de>
2015-04-18 22:21:43 +0200

    animations

--- scripts/player_bumping.gd
+++ scripts/player_bumping.gd
@@ -8,14 +8,22 @@ extends RigidBody2D
 var acc_x = 20
 var speed_x_max = 100
 var flying_mode = null
+var deflate = false
 
 func _ready():
 	# Initialization here
 	flying_mode = preload("res://character_flying.scn")
+	get_node("AnimationPlayer").play("inflate")
 	set_fixed_process(true)
 	pass
 
 func _fixed_process(delta):
+	if deflate and !get_node("AnimationPlayer").is_playing():
+		get_node("/root/root_node").set("transform_time", 2.0)
+		var fm = flying_mode.instance()
+		fm.set_pos(get_pos())
+		get_parent().add_child(fm)
+		self.queue_free()
 	get_node("/root/root_node").set("transform_time", get_node("/root/root_node").get("transform_time")-delta)
 	var current_vel = get_linear_velocity()
 	if Input.is_action_pressed("right") and current_vel.x < speed_x_max:
@@ -23,10 +31,7 @@ func _fixed_process(delta):
 	elif Input.is_action_pressed("left") and current_vel.x > -speed_x_max:
 		set_linear_velocity(vec2(current_vel.x - acc_x, current_vel.y))
 	if Input.is_action_pressed("mode_toggle") and get_node("/root/root_node").get("transform_time") <= 0:
-		get_node("/root/root_node").set("transform_time", 5.0)
-		var fm = flying_mode.instance()
-		fm.set_pos(get_pos())
-		get_parent().add_child(fm)
-		self.queue_free()
-	set_rot(0)
-	set_angular_velocity(0.0)
+		deflate = true
+		get_node("AnimationPlayer").play("deflate")
+#	set_rot(0)
+#	set_angular_velocity(0.0)
--- scripts/player_flying.gd
+++ scripts/player_flying.gd
@@ -27,10 +27,13 @@ func _fixed_process(delta):
 		set_linear_velocity(vec2(current_vel.x - acc_x, current_vel.y))
 	if Input.is_action_pressed("fly") and current_vel.y > -speed_y_max:
 		set_linear_velocity(vec2(current_vel.x, current_vel.y - acc_y))
+		get_node("AnimationPlayer").play("flap")
 		set_rot(0)
 		set_angular_velocity(0.0)
+	else:
+		get_node("AnimationPlayer")
 	if Input.is_action_pressed("mode_toggle") and get_node("/root/root_node").get("transform_time") <= 0:
-		get_node("/root/root_node").set("transform_time", 5.0)
+		get_node("/root/root_node").set("transform_time", 2.0)
 		var bm = bumping_mode.instance()
 		bm.set_pos(get_pos())
 		get_parent().add_child(bm)
